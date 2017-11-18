0323bd31f925afdca259c951515195716bd221b0
Mohammed Alshaboti <alshaboti.it@gmail.com>
2017-11-18 22:45:32 +1300

    Create servers_clients.sh

---
+++ demo/util/servers_clients.sh
@@ -0,0 +1,112 @@
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+
+############ Setup Link022 AP
+sudo ip netns add nsap
+sudo ip link add veth0 type veth peer name veth1
+sudo ip link set veth1 netns nsap
+
+#Add wlan1 to nsap
+sudo iw phy phy1 set netns "$(ip netns exec nsap sh -c 'sleep 1 >&- & echo "$!"')"
+
+sudo ip netns exec nsap ip addr add 192.168.11.2/24 dev veth1
+sudo ip netns exec nsap ip link set dev veth1 up
+sudo ip netns exec nsap ip link set dev wlan1 up
+
+############ Setup gateway
+# A utility script to start services on another machine, from which the operator
+# can manage the link022 device.
+# A typical setup is to create a peer to peer ethernet link between link022 and
+# the service machine.
+# Interface connected to the link022 host
+source servers.conf
+INTF=veth0
+OUT_INTF=wlan0  # Interface to connect to the Internet
+CTRL_INTF=eth0  # Interface to connect to a controlling machine, ssh is enabled here.
+GWIP=192.168.11.1
+
+sudo ip netns add ${NS}
+sudo ip link set dev ${INTF} netns ${NS}
+sudo ip netns exec ${NS} ip addr add ${GWIP}/24 dev ${INTF}
+sudo ip netns exec ${NS} ip link set dev ${INTF} up
+
+# Start DHCP
+sudo ip netns exec ${NS} dnsmasq --no-ping -p 0 -k \
+ -F set:s0,192.168.11.2,192.168.11.10 \
+ -O tag:s0,3,192.168.11.1 -O option:dns-server,8.8.8.8  -I lo -z \
+ -l /tmp/link022.leases -8 /tmp/link022.dhcp.log -i ${INTF} -a ${GWIP} --conf-file= &
+
+########### Get Internet access for link022
+TO_DEF=to_def
+TO_NS=to_${NS}
+
+# enable forwarding
+sudo sysctl net.ipv4.ip_forward=1
+sudo ip netns exec ${NS} sysctl net.ipv4.ip_forward=1
+
+# create veth pair
+sudo ip link add name ${TO_NS} type veth peer name ${TO_DEF} netns ${NS}
+# configure interfaces and routes
+sudo ip addr add 192.168.22.1/30 dev ${TO_NS}
+sudo ip link set ${TO_NS} up
+# sudo ip route add 192.168.22.0/30 dev ${TO_NS}
+sudo ip netns exec ${NS} ip addr add 192.168.22.2/30 dev ${TO_DEF}
+sudo ip netns exec ${NS} ip link set ${TO_DEF} up
+sudo ip netns exec ${NS} ip route add default via 192.168.22.1
+# NAT in LK22
+sudo ip netns exec ${NS} iptables -t nat -F
+sudo ip netns exec ${NS} iptables -t nat -A POSTROUTING -o ${TO_DEF} -j MASQUERADE
+# NAT in default
+sudo iptables -P FORWARD DROP
+sudo iptables -F FORWARD
+# Assuming the host does not have other NAT rules.
+sudo iptables -t nat -F
+sudo iptables -t nat -A POSTROUTING -s 192.168.22.0/30 -o ${OUT_INTF} -j MASQUERADE
+sudo iptables -A FORWARD -i ${OUT_INTF} -o ${TO_NS} -j ACCEPT
+sudo iptables -A FORWARD -i ${TO_NS} -o ${OUT_INTF} -j ACCEPT
+
+########### Adding vlans
+function add_vlan {
+	vlan_name=$1
+	vlan_id=$2
+	vlan_net=$3
+	vlan_gw=${vlan_net}.1
+	sudo ip netns exec ${NS} ip link add link ${INTF} name ${vlan_name} type vlan id ${vlan_id}
+	sudo ip netns exec ${NS} ip addr add ${vlan_gw}/24 dev ${vlan_name}
+	sudo ip netns exec ${NS} ip link set dev ${vlan_name} up
+
+	# Start DHCP
+	sudo ip netns exec ${NS} dnsmasq --no-ping -p 0 -k \
+	 -F set:s0,${vlan_net}.2,${vlan_net}.100 \
+	 -O tag:s0,3,${vlan_gw} -O option:dns-server,8.8.8.8  -I lo -z \
+	 -l /tmp/link022.${vlan_name}.leases -8 /tmp/link022.${vlan_name}.dhcp.log -i ${vlan_name} -a ${vlan_gw} --conf-file= &
+}
+add_vlan guest 200 192.168.33
+add_vlan auth 300 192.168.44
+
+RADIUS_PATH=../radius/freeradius
+sudo ip netns exec ${NS} freeradius -X -d ${RADIUS_PATH} > /tmp/radius.log &
+
+########### Set up controlling machine interface
+CTRL_NET=192.168.55
+CTRL_GW=${CTRL_NET}.1
+sudo ip addr add ${CTRL_GW}/24 dev ${CTRL_INTF}
+sudo ip link set dev ${CTRL_INTF} up
+# Start DHCP
+sudo dnsmasq --no-ping -p 0 -k \
+ -F set:s0,${CTRL_NET}.2,${CTRL_NET}.10 \
+ -O option:dns-server,8.8.8.8  -I lo -z \
+ -l /tmp/link022.ctrl.leases -8 /tmp/link022.dhcp.ctrl.log -i ${CTRL_INTF} -a ${CTRL_GW} --conf-file= &
